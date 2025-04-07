c9ecd2f74227a23d036b96505b9dd917e18ec4c9
Nikolas Siccha <nikolassiccha@gmail.com>
2025-04-07 12:39:51 +0200

    Add graceful handling of plots outside of ijulia.

---
+++ ext/PlotsExt.jl
@@ -0,0 +1,26 @@
+module PlotsExt
+
+import Plots, QuartoComponents
+
+const PLOTS_PATH = mktempdir()
+
+QuartoComponents.pretty(x::Plots.Plot) = if QuartoComponents.isijulia()
+    try
+        io = IOBuffer()
+        show(io, MIME("text/html"), x)
+        "\n" * replace(strip(String(take!(io))), r"\n\s+"=>"\n") * "\n"
+    catch
+        string(x)
+    end
+else
+    try
+        mkpath(PLOTS_PATH)
+        png_path = mktemp(PLOTS_PATH)[1] * ".png"
+        Plots.png(x, png_path)
+        "\n![]($png_path)\n"
+    catch
+        string(x)
+    end
+end
+
+end
